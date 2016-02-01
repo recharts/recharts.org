296dc92418e092d88b46b4812ab6f8a982c96d0a
arcthur <arthurtemptation@gmail.com>
2016-02-01 22:17:43 +0800

    fix: change version

--- src/views/IndexView.js
+++ src/views/IndexView.js
@@ -28,7 +28,7 @@ class IndexView extends Component {
         <div className="desc">
           <p className="title">Recharts = React + Charts</p>
           <p className="subtitle">Re-designed charting library built with React</p>
-          <p><a href="/guide/installation" className="button install-btn">Install v0.4.6</a></p>
+          <p><a href="/guide/installation" className="button install-btn">Install v0.4.7</a></p>
         </div>
         <div className="examples">
           <div className="ex-code">
