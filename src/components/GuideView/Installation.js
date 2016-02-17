c1acfc9c33d67d5be94c29685e08e9af78cf4313
arcthur <arthurtemptation@gmail.com>
2016-02-17 13:02:10 +0800

    feat: add umd installation

--- src/components/GuideView/Installation.js
+++ src/components/GuideView/Installation.js
@@ -11,6 +11,12 @@ const Installation = () => {
       {`  # latest stable
   $ npm install recharts`}
       </Highlight>
+      <h4>UMD</h4>
+      <p>The UMD build is also available on <a href="https://npmcdn.com/" target="_blank">npmcdn</a>:</p>
+      <Highlight className="html">
+{`  <script src="https://npmcdn.com/recharts/umd/Recharts.min.js"></script>`}
+      </Highlight>
+      <p>You can find the library on <code>window.Recharts</code></p>
       <h4>Dev Build</h4>
       <Highlight className="sh">
       {`  $ git clone https://github.com/recharts/recharts.git
