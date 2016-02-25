d3d11a25785912fb11107f4ebbee97d36f425c05
arcthur <arthurtemptation@gmail.com>
2016-02-25 12:43:44 +0800

    feat: add scroller-behavior

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
+import { useRouterHistory } from 'react-router';
+import useScroll from 'scroll-behavior/lib/useStandardScroll';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './redux/configureStore';
 import Root from './containers/Root';
 import routes from './routes';
 
-const store = configureStore(browserHistory);
-const history = syncHistoryWithStore(browserHistory, store);
+const createScrollHistory = useScroll(createBrowserHistory);
+const appHistory = useRouterHistory(createScrollHistory)();
+
+const store = configureStore(appHistory);
+const history = syncHistoryWithStore(appHistory, store);
 
 render(
   <Root store={store} history={history} routes={routes} />,
