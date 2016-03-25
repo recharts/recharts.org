5d3221b724111fc3511b17bfebacc7104a3ee0c7
arcthur <arthurtemptation@gmail.com>
2016-03-25 23:55:00 +0800

    deps: update deps

--- src/views/IndexView.js
+++ src/views/IndexView.js
@@ -32,7 +32,7 @@ class IndexView extends Component {
         <div className="desc">
           <p className="title">Recharts</p>
           <p className="subtitle">Redefined chart library built with React</p>
-          <p><a href="/guide#installation" className="button install-btn">Install v0.8.6</a></p>
+          <p><a href="/guide#installation" className="button install-btn">Install v0.8.8</a></p>
         </div>
         <div className="examples">
           <div className="ex-code">
