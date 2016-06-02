b89741a94b7089c4b54125ff82675c60f0ae32a7
xile611 <327197681@qq.com>
2016-06-02 20:19:29 +0800

    feat: add demo of AreaChart filled with linear gradient color

--- src/docs/api/AreaChart.js
+++ src/docs/api/AreaChart.js
@@ -14,29 +14,50 @@ const data = [
 const example = () => (
   <AreaChart width={730} height={250} data={data}
     margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+    <defs>
+      <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+      </linearGradient>
+      <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+      </linearGradient>
+    </defs>
     <XAxis dataKey="name" />
     <YAxis />
     <CartesianGrid strokeDasharray="3 3" />
     <Tooltip />
-    <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
-    <Area type='monotone' dataKey='pv' stroke='#8884d8' fill='#82ca9d' />
+    <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+    <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
   </AreaChart>
 );
 
 export default {
   name: 'AreaChart',
+  desc: 'All the svg elements can be added into an AreaChart, such as defs, linearGradient, etc.',
   examples: [
     {
       name: 'Simple AreaChart',
       demo: example,
       code: `<AreaChart width={730} height={250} data={data}
   margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+  <defs>
+    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+    </linearGradient>
+    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
   <XAxis dataKey="name" />
   <YAxis />
   <CartesianGrid strokeDasharray="3 3" />
   <Tooltip />
-  <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
-  <Area type='monotone' dataKey='pv' stroke='#8884d8' fill='#82ca9d' />
+  <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+  <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
 </AreaChart>
 `,
     },
