ac6662007372aaee9fa2fc2155bbdd621cf8b593
xile611 <327197681@qq.com>
2016-12-31 14:27:50 +0800

    feat: add design disciplines

---
+++ src/components/GuideView/Disciplines.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Highlight from 'utils/Highlight';
+import CustomAxisTick from './CustomAxisTick';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import { localeGet } from '../../utils/LocaleUtils';
+
+
+const GettingStarted = ({ locale }) => (
+  <div className="mod-design-disciplines" id="Design_Disciplines">
+    <h3></h3>
+  </div>
+);
+
+export default GettingStarted;
