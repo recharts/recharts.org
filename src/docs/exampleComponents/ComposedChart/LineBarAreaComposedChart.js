204e2e1d68000a27a7e08d29d2cf448581364c96
xile611 <327197681@qq.com>
2017-11-24 17:33:12 +0800

    feat: add three demos of ScatterChart

--- src/docs/exampleComponents/ComposedChart/LineBarAreaComposedChart.js
+++ src/docs/exampleComponents/ComposedChart/LineBarAreaComposedChart.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { ComposedChart, Line, Area, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
-  Legend } from 'recharts';
+  Legend, Scatter } from 'recharts';
 
 const data = [
-  {name: 'Page A', uv: 590, pv: 800, amt: 1400},
-  {name: 'Page B', uv: 868, pv: 967, amt: 1506},
-  {name: 'Page C', uv: 1397, pv: 1098, amt: 989},
-  {name: 'Page D', uv: 1480, pv: 1200, amt: 1228},
-  {name: 'Page E', uv: 1520, pv: 1108, amt: 1100},
-  {name: 'Page F', uv: 1400, pv: 680, amt: 1700},
+  {name: 'Page A', uv: 590, pv: 800, amt: 1400, cnt: 490},
+  {name: 'Page B', uv: 868, pv: 967, amt: 1506, cnt: 590},
+  {name: 'Page C', uv: 1397, pv: 1098, amt: 989, cnt: 350},
+  {name: 'Page D', uv: 1480, pv: 1200, amt: 1228, cnt: 480},
+  {name: 'Page E', uv: 1520, pv: 1108, amt: 1100, cnt: 460},
+  {name: 'Page F', uv: 1400, pv: 680, amt: 1700, cnt: 380},
 ];
 
 export default class Example extends Component {
@@ -26,6 +26,7 @@ export default class Example extends Component {
         <Area type='monotone' dataKey='amt' fill='#8884d8' stroke='#8884d8'/>
         <Bar dataKey='pv' barSize={20} fill='#413ea0'/>
         <Line type='monotone' dataKey='uv' stroke='#ff7300'/>
+        {/* <Scatter dataKey="cnt" fill="red" /> */}
       </ComposedChart>
     );
   }
