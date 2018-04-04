650b80a2d9bee8986d06a4be7ec1d837e8c8302e
xile611 <327197681@qq.com>
2018-04-04 10:26:02 +0800

    feat: update users and url

--- src/docs/users/users.js
+++ src/docs/users/users.js
@@ -34,4 +34,39 @@ export default [
     url: 'https://www.tracktl.com/',
     logoImgUrl: '//res.cloudinary.com/jukeo-net/image/upload/c_pad,h_100,q_100,w_300/v1509094068/logo-w_copie_tiweid.png'
   },
+  {
+    name: 'CineSend',
+    url: 'https://cinesend.com/',
+    logoImgUrl: 'https://camo.githubusercontent.com/7c700a8cdcabf704107a36854ce02cedad386914/68747470733a2f2f62697463696e6573746f726167656163636f756e742e626c6f622e636f72652e77696e646f77732e6e65742f63696e6573656e642d6173736574732f6c6f676f732f63696e6573656e645f72656368617274735f6c6f676f2e706e67',
+  },
+  {
+    name: 'Demisto',
+    url: 'https://www.demisto.com/',
+    logoImgUrl: 'https://user-images.githubusercontent.com/15967181/32991200-9ce4f902-cd3f-11e7-9e05-a86ebabb151c.jpeg',
+  },
+  {
+    name: 'Rebla',
+    url: 'http://boka-objekt.se/',
+    logoImgUrl: 'https://camo.githubusercontent.com/3698cb94ed0cff03b8c87cffe4db1399f6931769/68747470733a2f2f73332e65752d63656e7472616c2d312e616d617a6f6e6177732e636f6d2f626f6b612d6f626a656b742f7265626c612f6c6f676f2e706e67',
+  },
+  {
+    name: 'Squadlytics',
+    url: 'https://squadlytics.com',
+    logoImgUrl: 'https://camo.githubusercontent.com/232ceeb7bb378384257720e448a449f72ab95c18/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f73717561642d7374617469632f72656368617274732d6c6f676f2e706e67',
+  },
+  {
+    name: 'ahrefs',
+    url: 'https://ahrefs.com/',
+    logoImgUrl: 'https://img.alicdn.com/tfs/TB1zGiTiFmWBuNjSspdXXbugXXa-640-244.png',
+  },
+  {
+    name: 'Yety',
+    url: 'http://yety.io',
+    logoImgUrl: 'https://camo.githubusercontent.com/d45641bb2001f297174337a9514f34dfe928cbf8/68747470733a2f2f73332d75732d776573742d322e616d617a6f6e6177732e636f6d2f796574792d6170702f736f6369616c2f796574792d72656368617274732e706e67',
+  },
+  {
+    name: 'Metric.ai',
+    url: 'https://www.metric.ai',
+    logoImgUrl: 'https://camo.githubusercontent.com/d99b8679214e490052509042808bbe14630ed432/68747470733a2f2f7777772e6d65747269632e61692f66696c65732f6272616e642f6c6f676f2f6d65747269632d61692d333030783130302d6c696768742e706e67',
+  },
 ]
