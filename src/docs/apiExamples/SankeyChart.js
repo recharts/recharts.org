0e03bb1a6e01aa29804190dea5bdb589076a66d7
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-12-09 10:25:05 -0300

    IMprove formatting

--- src/docs/apiExamples/SankeyChart.js
+++ src/docs/apiExamples/SankeyChart.js
@@ -86,20 +86,20 @@ const example = () => (
 
 const exampleCode = `
 <Sankey
-width={960}
-height={500}
-data={data0}
-node={<MyCustomNode />}
-nodePading={50}
-margin={{
-  left: 200,
-  right: 200,
-  top: 100,
-  bottom: 100,
-}}
-link={{ stroke: '#77c878' }}
+  width={960}
+  height={500}
+  data={data0}
+  node={<MyCustomNode />}
+  nodePading={50}
+  margin={{
+   left: 200,
+    right: 200,
+    top: 100,
+    bottom: 100,
+  }}
+  link={{ stroke: '#77c878' }}
 >
-<Tooltip />
+  <Tooltip />
 </Sankey>
 `;
 
