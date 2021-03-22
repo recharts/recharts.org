42606fa1efca49c313b871fb5de62183889085f8
arcthur <arthurtemptation@gmail.com>
2021-03-22 21:59:42 +0800

    optimize the output

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -77,6 +77,9 @@ module.exports = {
       new CssMinimizerPlugin(),
       new TerserPlugin(),
     ],
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   plugins: [
     new MonacoWebpackPlugin({
