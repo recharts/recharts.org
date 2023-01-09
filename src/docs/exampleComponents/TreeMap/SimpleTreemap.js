59ac7b37fa7451316cbfa63e4158fed6a0536448
Coltin Kifer <ckifer@amazon.com>
2023-01-09 15:53:06 -0800

    fix: treemap ratio incorrect prop

--- src/docs/exampleComponents/TreeMap/SimpleTreemap.js
+++ src/docs/exampleComponents/TreeMap/SimpleTreemap.js
@@ -135,12 +135,12 @@ const data = [
 ];
 
 export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/simple-treemap-r5o4e';
+  static demoUrl = 'https://codesandbox.io/s/simple-treemap-aspect-bt7w40';
 
   render() {
     return (
       <ResponsiveContainer width="100%" height="100%">
-        <Treemap width={400} height={200} data={data} dataKey="size" ratio={4 / 3} stroke="#fff" fill="#8884d8" />
+        <Treemap width={400} height={200} data={data} dataKey="size" aspectRatio={4 / 3} stroke="#fff" fill="#8884d8" />
       </ResponsiveContainer>
     );
   }
