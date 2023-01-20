0dc991d6a8ef1b74e842e4dc81c71e848f7f605c
Coltin Kifer <coltinkifer19@gmail.com>
2023-01-20 10:00:36 -0800

    fix: two broken examples, use function components for parity with codesandbox, update to 2.3.2 (#242)
    Co-authored-by: Coltin Kifer <ckifer@amazon.com>

--- docs/examples/ScatterChart/ScatterChartWithCells.js
+++ docs/examples/ScatterChart/ScatterChartWithCells.js
@@ -1,9 +1,5 @@
-import React, { PureComponent } from 'react';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Cell, ResponsiveContainer } from 'recharts';
-import { scaleOrdinal } from 'd3-scale';
-import { schemeCategory10 } from 'd3-scale-chromatic';
-
-const colors = scaleOrdinal(schemeCategory10).range();
+import React from 'react';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Cell } from 'recharts';
 
 const data = [
   { x: 100, y: 200, z: 200 },
@@ -13,32 +9,31 @@ const data = [
   { x: 150, y: 400, z: 500 },
   { x: 110, y: 280, z: 200 },
 ];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
-export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/scatter-chart-with-cells-2sk2o';
-
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height={400}>
-        <ScatterChart
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 20,
-            left: 20,
-          }}
-        >
-          <CartesianGrid />
-          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter name="A school" data={data} fill="#8884d8">
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-            ))}
-          </Scatter>
-        </ScatterChart>
-      </ResponsiveContainer>
-    );
-  }
+export default function App() {
+  return (
+    <ScatterChart
+      width={400}
+      height={400}
+      margin={{
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20,
+      }}
+    >
+      <CartesianGrid />
+      <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+      <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+      <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+      <Scatter name="A school" data={data} fill="#8884d8">
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Scatter>
+    </ScatterChart>
+  );
 }
+
+App.demoUrl = 'https://codesandbox.io/s/scatter-chart-with-cells-2sk2o';
