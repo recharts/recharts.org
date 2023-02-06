ab0d57a2627e2a13aef44e0b3d4d51e43bd7902a
lukask-proxora <102710733+lukask-proxora@users.noreply.github.com>
2023-02-06 01:17:30 +0100

    update shown code for scatter chart example to match actual example code (#251)

--- src/docs/apiExamples/ScatterChart.js
+++ src/docs/apiExamples/ScatterChart.js
@@ -42,12 +42,20 @@ const example = () => (
 );
 
 const exampleCode = `
-<ScatterChart width={730} height={250}
-  margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+<ScatterChart
+  width={730}
+  height={250}
+  margin={{
+    top: 20,
+    right: 20,
+    bottom: 10,
+    left: 10,
+  }}
+>
   <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="x" name="stature" unit="cm" />
-  <YAxis dataKey="y" name="weight" unit="kg" />
-  <ZAxis dataKey="z" range={[64, 144]} name="score" unit="km" />
+  <XAxis dataKey="x" type="number" name="stature" unit="cm" />
+  <YAxis dataKey="y" type="number" name="weight" unit="kg" />
+  <ZAxis dataKey="z" type="number" range={[64, 144]} name="score" unit="km" />
   <Tooltip cursor={{ strokeDasharray: '3 3' }} />
   <Legend />
   <Scatter name="A school" data={data01} fill="#8884d8" />
