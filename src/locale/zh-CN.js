318a3787db4fa329e519c466497238d76ffab8de
Coltin Kifer <ckifer@amazon.com>
2023-02-06 11:41:08 -0800

    fix: address pr comments, add css, remove unndeeded connect functionality

--- src/locale/zh-CN.js
+++ src/locale/zh-CN.js
@@ -34,7 +34,7 @@ const map = {
     api: 'API',
     examples: '示例',
     blog: '博客',
-    storybook: 'Storybook (WIP)',
+    storybook: 'Storybook (工作正在进行中)',
     contract: '联系我们',
   },
   guide: {
