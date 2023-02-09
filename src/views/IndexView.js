e39c73b9bf16041fd8033bfdcec350d58ee17b55
Coltin Kifer <ckifer@amazon.com>
2023-02-09 10:14:50 -0800

    chore: increment recharts version and build

--- src/views/IndexView.js
+++ src/views/IndexView.js
@@ -69,7 +69,7 @@ class IndexView extends PureComponent {
             <Link to={`/${locale}/guide/installation`} className="button install-btn">
               <i className="icon-energy" />
               {localeGet(locale, 'home', 'install')}
-              &nbsp;v2.3.2
+              &nbsp;v2.4.0
             </Link>
           </p>
           <iframe
