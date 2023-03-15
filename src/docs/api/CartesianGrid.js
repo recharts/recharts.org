7e47f98c6628e1a7c2da706a319dbd919c57fff8
lukask-proxora <102710733+lukask-proxora@users.noreply.github.com>
2023-03-15 17:56:25 +0100

    add docs for coordinate generators of CartesianGrid (#260)
    * add docs for coordinate generators of CartesianGrid
    
    * add chinese translations

--- src/docs/api/CartesianGrid.js
+++ src/docs/api/CartesianGrid.js
@@ -72,6 +72,25 @@ export default {
       },
     },
     {
+      name: 'horizontalCoordinatesGenerator',
+      type: 'Function',
+      isOptional: true,
+      desc: {
+        'en-US': 'A function that generates the y-coordinates of all horizontal lines. The generator gets passed an object of the shape { yAxis, width, height, offset }.',
+        'zh-CN': '生成所有水平线的 y 坐标的函数。生成器传递一个形状为 { yAxis, width, height, offset } 的对象。',
+      },
+      format: [
+        '<CartesianGrid strokeDasharray="3 3" horizontalCoordinatesGenerator={(props) => props.height > 250 ? [75, 150, 225] : [100, 200]} />',
+      ],
+      examples: [
+        {
+          name: 'Cartesian grid with coordinate generators',
+          url: 'https://codesandbox.io/s/cartesian-grid-with-coordinate-generators-my38cg',
+          isExternal: true,
+        },
+      ],
+    },
+    {
       name: 'verticalPoints',
       type: 'Array',
       defaultVal: '[]',
@@ -80,6 +99,25 @@ export default {
         'en-US': 'The x-coordinates of all vertical lines.',
         'zh-CN': '所有竖直网格线的 x 坐标。',
       },
+    },
+    {
+      name: 'verticalCoordinatesGenerator',
+      type: 'Function',
+      isOptional: true,
+      desc: {
+        'en-US': 'A function that generates the x-coordinates of all vertical lines. The generator gets passed an object of the shape { xAxis, width, height, offset }.',
+        'zh-CN': '生成所有垂直线的 x 坐标的函数。生成器传递一个形状为 { xAxis, width, height, offset } 的对象',
+      },
+      format: [
+        '<CartesianGrid strokeDasharray="3 3" verticalCoordinatesGenerator={(props) => props.width > 450 ? [150, 300, 450] : [200, 400]} />',
+      ],
+      examples: [
+        {
+          name: 'Cartesian grid with coordinate generators',
+          url: 'https://codesandbox.io/s/cartesian-grid-with-coordinate-generators-my38cg',
+          isExternal: true,
+        },
+      ],
     },
     {
       name: 'strokeDasharray',
