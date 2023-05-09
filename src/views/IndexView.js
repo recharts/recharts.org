d05a95aac074d752cb651fba7dfc9d4ed6eeaa16
Coltin Kifer <ckifer@amazon.com>
2023-05-09 16:53:26 -0700

    chore: build, bump version

--- src/views/IndexView.js
+++ src/views/IndexView.js
@@ -69,7 +69,7 @@ class IndexView extends PureComponent {
             <Link to={`/${locale}/guide/installation`} className="button install-btn">
               <i className="icon-energy" />
               {localeGet(locale, 'home', 'install')}
-              &nbsp;v2.5.0
+              &nbsp;v2.6.0
             </Link>
           </p>
           <iframe
