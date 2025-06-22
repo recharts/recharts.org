d5fb1b9b7139af63f729159328a1676d38420a0b
Pavel Vanecek <corkscreewe@gmail.com>
2025-06-22 19:49:21 +0900

    Move TriangleBar to tsx

--- src/components/GuideView/TriangleBar.tsx
+++ src/components/GuideView/TriangleBar.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const getPath = (x, y, width, height) =>
+const getPath = (x: number, y: number, width: number, height: number) =>
   `M${x},${y + height}
    C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
    Z`;
 
-const TriangleBar = (props) => {
+type TriangleBarProps = {
+  fill?: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+const TriangleBar = (props: TriangleBarProps) => {
   const { fill, x, y, width, height } = props;
 
   return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-};
-
-TriangleBar.propTypes = {
-  fill: PropTypes.string,
-  x: PropTypes.number,
-  y: PropTypes.number,
-  width: PropTypes.number,
-  height: PropTypes.number,
 };
 
 export default TriangleBar;
