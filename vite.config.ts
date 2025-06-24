8e0b68f52ad6eb7accd5ae43ebbfba0addbdd926
Pavel Vanecek <corkscreewe@gmail.com>
2025-06-24 18:11:27 +0900

    Add Codecov bundler plugin
    https://docs.codecov.com/docs/vite-quick-start

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import * as path from 'path';
+import { codecovVitePlugin } from '@codecov/vite-plugin';
 
 export default defineConfig({
   // depending on your application, base can also be "/"
@@ -16,6 +17,15 @@ export default defineConfig({
       },
     }),
     viteTsconfigPaths(),
+    /*
+     * Put the Codecov Vite plugin after all other plugins
+     * https://docs.codecov.com/docs/vite-quick-start
+     */
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: 'recharts.org',
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
   ],
   server: {
     // this ensures that the browser opens upon server start
