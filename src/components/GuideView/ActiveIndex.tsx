441744f2dd7eb09e99516fa2d3c70bd8b24d3f27
Pavel Vanecek <corkscreewe@gmail.com>
2025-07-02 21:40:04 +0900

    Move ul outside of p

--- src/components/GuideView/ActiveIndex.tsx
+++ src/components/GuideView/ActiveIndex.tsx
@@ -22,27 +22,25 @@ export function ActiveIndex({ locale }: { locale: SupportedLocale }) {
         <Link to={`/${locale}/api/Tooltip`}>Tooltip</Link> component is controlling user interaction with the chart. Use
         Tooltip!
       </p>
-      <p>
-        Tooltip has several props that allow you to control the interaction in detail:
-        <ul>
-          <li>
-            <code>defaultIndex</code>: Sets the initial index of the item that is highlighted when the chart is
-            rendered, before any user interactions
-          </li>
-          <li>
-            <code>active</code>: If true, the tooltip remains active even when the user interaction has completed (for
-            example, when user hovers over a different item)
-          </li>
-          <li>
-            <code>content</code>: This prop decides what content is displayed in the tooltip. You can turn off the
-            rendering completely by passing <code>{`content={() => null}`}</code>.
-          </li>
-          <li>
-            <code>cursor</code>: Is what renders in the plot area, to draw attention to the item that is being
-            interacted with. You can turn it off by passing <code>cursor={false}</code>.
-          </li>
-        </ul>
-      </p>
+      <p>Tooltip has several props that allow you to control the interaction in detail:</p>
+      <ul>
+        <li>
+          <code>defaultIndex</code>: Sets the initial index of the item that is highlighted when the chart is rendered,
+          before any user interactions
+        </li>
+        <li>
+          <code>active</code>: If true, the tooltip remains active even when the user interaction has completed (for
+          example, when user hovers over a different item)
+        </li>
+        <li>
+          <code>content</code>: This prop decides what content is displayed in the tooltip. You can turn off the
+          rendering completely by passing <code>{`content={() => null}`}</code>.
+        </li>
+        <li>
+          <code>cursor</code>: Is what renders in the plot area, to draw attention to the item that is being interacted
+          with. You can turn it off by passing <code>cursor={false}</code>.
+        </li>
+      </ul>
       <h2>Example 1: PieChart with default index</h2>
       <p>
         The example below shows how to use the <code>defaultIndex</code> prop to set the initial item that is
