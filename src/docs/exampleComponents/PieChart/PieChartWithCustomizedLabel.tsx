25c9ae1880e8dc58f63c480ea78c2dad9a7a7c23
Coltin Kifer <ckifer@atlassian.com>
2025-09-08 17:47:44 -0700

    fix: type errors so 3.2 can deploy

--- src/docs/exampleComponents/PieChart/PieChartWithCustomizedLabel.tsx
+++ src/docs/exampleComponents/PieChart/PieChartWithCustomizedLabel.tsx
@@ -1,39 +1,5 @@
 import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts';
 
-type TooltipPayload = ReadonlyArray<any>;
-
-type Coordinate = {
-  x: number;
-  y: number;
-};
-
-type PieSectorData = {
-  percent?: number;
-  name?: string | number;
-  midAngle?: number;
-  middleRadius?: number;
-  tooltipPosition?: Coordinate;
-  value?: number;
-  paddingAngle?: number;
-  dataKey?: string;
-  payload?: any;
-  tooltipPayload?: ReadonlyArray<TooltipPayload>;
-};
-
-type GeometrySector = {
-  cx: number;
-  cy: number;
-  innerRadius: number;
-  outerRadius: number;
-  startAngle: number;
-  endAngle: number;
-};
-
-type PieLabelProps = PieSectorData &
-  GeometrySector & {
-    tooltipPayload?: any;
-  };
-
 const data = [
   { name: 'Group A', value: 400 },
   { name: 'Group B', value: 300 },
@@ -44,7 +10,7 @@ const data = [
 const RADIAN = Math.PI / 180;
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: PieLabelProps) => {
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: any) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-(midAngle ?? 0) * RADIAN);
   const y = cy + radius * Math.sin(-(midAngle ?? 0) * RADIAN);
